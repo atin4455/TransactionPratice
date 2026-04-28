5a6685e88cc6fe4d63fc4af17e2ff0a7d724b6be
祖程 <atinzulin@hotmail.com>
2026-04-28 13:39:43 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransactionPratice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
